214ff289c632666fb13f2c72e973ef7ddf6f1b90
Shinyoung Kim <rolroralra@gmail.com>
2024-05-31 18:26:35 +0900

    feat: 수정 기능 추가

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [1, 'always', 5000],
-    'footer-max-line-length': [1, 'always', 5000]
+    'body-max-line-length': [1, 'always', 5000],  // 5000 is the maximum line length
+    'footer-max-line-length': [1, 'always', 5000] // 5000 is the maximum line length
   },
 };
