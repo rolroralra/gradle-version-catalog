7a2deb2eb0bfd33e89a9698b3627f6731e2d8825
Shinyoung Kim <rolroralra@gmail.com>
2024-05-31 18:14:29 +0900

    Docs/20240531 (#4)
    * feat: libs.versions.toml 수정
    
    * fix: commitlint.confg.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 5000]
+    'body-max-line-length': [1, 'always', 5000],
+    'footer-max-line-length': [1, 'always', 5000]
   },
 };
